c74feec8b009e32a9cfa421172c55dc6cf4b2a1a
김도영 <binj1246@gmail.com>
2025-03-21 13:45:57 +0900

    first commit

---
+++ composeApp/src/iosMain/kotlin/com/sinjidragon/turlgo/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.sinjidragon.turlgo
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ composeApp/src/iosMain/kotlin/com/sinjidragon/turlgo/Platform.ios.kt
@@ -0,0 +1,9 @@
+package com.sinjidragon.turlgo
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
